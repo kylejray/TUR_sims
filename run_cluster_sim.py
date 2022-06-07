91fd281dbd2a28e341f42432961b93a7148fb543
Kyle J Ray <kjray@Gorath.local>
2022-06-07 16:55:56 -0700

    imac cluster script added

--- run_cluster_sim.py
+++ run_cluster_sim.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 from SimRunner import TurRunner
+import math
 
 # always include these lines of MPI code!
 from mpi4py import MPI
@@ -10,36 +11,62 @@ size = comm.Get_size()  # number of MPI procs
 rank = comm.Get_rank()  # i.d. for local proc
 
 
-defaults = {'localization':18., 'location':.5, 'depth':3, 'tilt':2., 'beta':1., 'tau':1., 'scale':1., 'dt':1/10000, 'lambda':1, 'N':10_000}
+#defaults = {'localization':18., 'location':.5, 'depth':3, 'tilt':2., 'beta':1., 'tau':1., 'scale':1., 'dt':1/10000, #'lambda':1, 'N':10_000}
 
-N=100_000
-dt = 1/20_000
+initial_params = {'N':256_000,'dt':1/10_000,'target_work':1, 'tau':3, 'depth':3, 'tilt':2}
+
 
 def save_func(self):
-   N = int(self.params['N']/1000)
-   tau = int(100*self.params['tau'])
-   return [f'N{N}tau{tau:03d}', None]
+   loc = int(100*self.params['location'])
+   local = int(self.params['localization'])
+   return [f'loc{loc:03d}local{local:03d}', 'width_Sweep/']
 
-simrun = TurRunner(params = defaults)
+simrun = TurRunner()
 simrun.save_name = save_func
-simrun.change_params({'N':N,'dt':dt})
+simrun.change_params(initial_params)
 
 #run_name = 'heatmap_sym_detail_2X20L_5X8g_p16p31_N40/'
 #save_dir = '/home/kylejray/FQ_sims/results/{}/'.format(run_name)
+
+
 
 #max_L = 8
 #L_lists = [ L_range[i*max_L:(i+1)*max_L] for i in range(math.ceil(len(L_range)/max_L))]
 
 
+
 # my_param = params[rank]
 
-n = [1_000, 4_000, 16_000, 64_000, 256_000, 512_000] [rank]
-simrun.change_params({'N': n})
-# save parameters to output file by printing
-sys.stdout.write('my rank:{} of {}, N={} '.format(rank+1, size, simrun.params['N']))
-# perform local computation using local param
-simrun.run_sim()
-# save your results
-simrun.save_sim()
+#dt = [1/item for item in [500, 1_000, 5_000, 10_000, 15_000, 20_000, 30_000]][rank]
+#n = [item*1000 for item in [1,4,16,64,256,512]][rank]
 
-sys.stdout.flush()
+'''
+locals = [ 20, 50, 100, 200]
+zs = [ 2, 3, 4, 5, 6]
+combos=[]
+for l in locals:
+   for z in zs:
+      loc = z/np.sqrt(2*l)
+      combos.append({'localization':l, 'location':loc})
+
+print(len(combos))
+c_lists = [ combos[i*size:(i+1)*size] for i in range(math.ceil(len(combos)/size))]
+
+for combos in c_lists:
+   params = combos[rank]
+'''
+params = {'location':.5}
+locals = np.linspace(20,200,16) 
+l_lists = [ locals[i*size:(i+1)*size] for i in range(math.ceil(len(locals)/size))]
+for local_vals in l_lists:
+   params['localization'] = local_vals[rank]
+   simrun.change_params(params)
+
+   # save parameters to output file by printing
+   sys.stdout.write('my rank:{} of {}, params={} '.format(rank+1, size, params))
+   # perform local computation using local param
+   simrun.run_sim()
+   # save your results
+   simrun.save_sim()
+
+   sys.stdout.flush()
